7ce83c20cde8c4fe2fb8ecf4f0a6dc334367f7f8
rexxwei <wei.k@northeastern.edu>
2021-04-24 23:08:02 -0400

    update test.py

--- test.py
+++ test.py
@@ -6,16 +6,4 @@ This program is to test the function of each model's functionality.
 # import display as dis
 # import solution as sol
 
-# if __name__ == "__main__":
-# 	boa = sio.start_game()					# obtain data to start game
-# 	dis.draw_borad(boa)						# display the board
-# 	print("<===================>")			# separation line
-# 	show_sol = "n"
-# 	# accept input from user to decide show solution or not
-# 	while str(show_sol).lower() != "y" and str(show_sol).lower() != "yes":
-# 		show_sol = input("Want to see solution?[Y/N]: ")
-# 	print("<===================>")
-# 	sol.result(boa)							# call model to solve the puzzle
-# 	dis.draw_borad(boa)						# display the board
-# 	sio.output_game(boa)					# save data to txt file
-
+# there is no unit test set right now
